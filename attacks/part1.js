ff85a8400e3ef86e69c9b82b0ea8cef257e3670a
Asmi K <168493757+asmik12@users.noreply.github.com>
2024-11-10 23:03:23 +0000

    Implemented part1

--- attacks/part1.js
+++ attacks/part1.js
@@ -1,4 +1,8 @@
 // Attack 1: Automating dragging to halfway along the slider
 
 // Send a message to the React app to move the slider to 90 degrees (halfway)
-window.postMessage({ type: 'moveSlider', value: 90 }, '*');
+
+//Individually paste the below two lines into the console (not together) to bypass the captcha.
+document.querySelector('input[type="range"]').value = 90;
+
+document.querySelector('button').click();   
